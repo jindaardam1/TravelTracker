33cfe4c560faac7524acf18bdd690de484b12813
jindaardam1 <jindaardam1@gmail.com>
2024-01-25 10:23:15 +0100

    Eliminadas carpetas innecesarias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.traveltracker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.traveltracker"
