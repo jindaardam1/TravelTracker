e362c6bcf2b5182b2f34204bac11cdf1c5f04cdc
Midgard <malmeidamdam1@gmail.com>
2024-01-29 10:29:25 +0100

    Cambio layout en Register y ordenado
    pasa a ser constraint y se le ha añadido el logo de la app

--- app/src/main/java/com/example/traveltracker/LoginActivity.kt
+++ app/src/main/java/com/example/traveltracker/LoginActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.login)
     }
 }
