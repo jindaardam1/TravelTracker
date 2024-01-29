9aae820f9778847a75491677646bb2e178a092b2
Midgard <malmeidamdam1@gmail.com>
2024-01-29 12:47:56 +0100

    Primera versión/bosquejo perfil

---
+++ app/src/main/java/com/example/traveltracker/perfil.kt
@@ -0,0 +1,11 @@
+package com.example.traveltracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class perfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
