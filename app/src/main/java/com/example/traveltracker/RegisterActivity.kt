de9353fbb7ece82e59a61c53b63274bc8d87111a
Midgard <malmeidamdam1@gmail.com>
2024-02-21 13:50:44 +0100

    Añadido un autocomplete para el registro de paises en lugar del Spinner

--- app/src/main/java/com/example/traveltracker/RegisterActivity.kt
+++ app/src/main/java/com/example/traveltracker/RegisterActivity.kt
@@ -2,11 +2,21 @@ package com.example.traveltracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val countries = resources.getStringArray(R.array.paises)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, countries)
+
+        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextViewCountry)
+        autoCompleteTextView.setAdapter(adapter)
+
+
 
     }
 }
