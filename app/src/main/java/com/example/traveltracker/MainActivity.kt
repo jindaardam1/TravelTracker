58b491ea4e4ff6985275a2b4f5a6c4ecf9c9bc5c
Dandu35 <danduezfer@educacion.navarra.es>
2024-02-22 10:35:12 +0100

    Añadido el recycledView (falla en mostrarse) sin embargo con LOGCAT se ve como filtra los paises.

--- app/src/main/java/com/example/traveltracker/MainActivity.kt
+++ app/src/main/java/com/example/traveltracker/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.MapFragment
+
 
 class MainActivity : AppCompatActivity() {
 
