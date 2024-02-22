4a96df84ddb1cb683c2567a1d00a98ff8fd6bbdd
mpuentebdam1 <mpuentebdam1@gmail.com>
2024-02-22 10:56:30 +0100

    Arreglado error register y añadidas imagenes a navbar (cambiar por iconos)

--- app/src/main/java/com/example/traveltracker/RegisterActivity.kt
+++ app/src/main/java/com/example/traveltracker/RegisterActivity.kt
@@ -55,8 +55,10 @@ class RegisterActivity : AppCompatActivity() {
             {
                 Toast.makeText(this, "TODO CORRECTO", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+                // Funciona con esto, puede que no sea la mejor forma de hacerlo
                 finish()
+                startActivity(intent)
+
 
             }
 
