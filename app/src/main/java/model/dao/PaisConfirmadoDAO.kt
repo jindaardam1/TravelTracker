e93111b65392caef8aedbde3dc3d99f8369223b1
jindaardam1 <jindaardam1@gmail.com>
2024-02-28 10:31:10 +0100

    Añadidas las funciones para comprobar si un país está confirmado y para insertar una confirmación

--- app/src/main/java/model/dao/PaisConfirmadoDAO.kt
+++ app/src/main/java/model/dao/PaisConfirmadoDAO.kt
@@ -1,4 +1,19 @@
 package model.dao
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import model.entity.PaisConfirmado
+
 interface PaisConfirmadoDAO {
+    @Query("SELECT COUNT(pc.cod_confirmado) FROM estado_paises ep LEFT JOIN paises_confirmados pc ON ep.cod_pais = pc.cod_pais WHERE ep.nombre_pais = :codPais")
+    fun esConfirmado(codPais: String): Boolean {
+        return (esConfirmadoCount(codPais) > 0)
+    }
+
+    @Query("SELECT COUNT(pc.cod_confirmado) FROM estado_paises ep LEFT JOIN paises_confirmados pc ON ep.cod_pais = pc.cod_pais WHERE ep.nombre_pais = :codPais")
+    fun esConfirmadoCount(codPais: String): Int
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPaisConfirmado(paisConfirmado: PaisConfirmado)
 }
